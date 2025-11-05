627677616bdf87409ba003d25016fe7b800c3252
Paul Lindén <linden.91.pl@gmail.com>
2025-11-05 19:25:44 +0100

    removed unused endpoint

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,7 +4,6 @@ import productController from '../controllers/productController.js';
 const router = express.Router();
 
 // Routes
-router.get('/stores', productController.getAllStores);
 router.get('/stores/:storeId/config', productController.getStoreConfig);
 router.post('/calculate-price', productController.calculatePrice);
 
